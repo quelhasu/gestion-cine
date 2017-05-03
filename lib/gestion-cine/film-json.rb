ed5b6897b3f3a32b0ba41891833802120bd5713c
Unknown <uquelhas@gmail.com>
2017-05-03 02:43:22 +0200

    Film Json (comm)
    Ajout de commentaires.

--- lib/gestion-cine/film-json.rb
+++ lib/gestion-cine/film-json.rb
@@ -1,11 +1,18 @@
 module GestionCine
 
-
+  # Module permettant de creer un film a partir d une ligne envoyee
+  # et permet  de transformer un objet Film en hashmap pour pouvoir
+  # l exporter dans un fichier Json.
+  #
   module FilmJson
-
+    # Separateur pour les jours de diffusions d un film.
     SEPARATEUR_JOURS = "-"
 
     # Methode pour creer un objet Film pour chaque ligne presente dans le depot
+    # a partir des lignes presentes dans le fichier Json.
+    #
+    # @param [hashmap] ligne - ligne presente sous forme de hash.
+    #
     def self.creer_film( ligne )
       imdbid = ligne["imdbid"]
       jours = ligne["jours"]
@@ -14,12 +21,15 @@ module GestionCine
       Film.new(imdbid, salles , jours)
     end
 
-    # Methode pour renvoyer un objet Cours au format JSON
+    # Methode pour renvoyer un objet Film au format JSON.
+    #
+    # @param [Film] film - objet film a transformer en objet hashmap.
+    #
     def self.transformer( film )
       jours = film.jours
       salles = film.salles
       id = film.imdbid
-      a = {:imdbid => id, :jours => jours, :salles => salles}
+      hash = {:imdbid => id, :jours => jours, :salles => salles}
     end
 
   end
