f5d2d5379afa75424810591a3a525dc48acd939a
Unknown <uquelhas@gmail.com>
2017-05-03 03:03:33 +0200

    Motifs (comm)
    Ajout de commentaires.

--- lib/gestion-cine/motifs.rb
+++ lib/gestion-cine/motifs.rb
@@ -1,12 +1,16 @@
 module GestionCine
 
+  #
+  # Module permettant de retrouver les differents Motifs
+  # que l on peut retrouver dans une base de donnees.
+  #
   module Motifs
     ID =  %r{\btt[0-9]{7}\b}
     JOUR = %r{\b[A-Z][a-z]{,7}\b}
     JOURS = %r{\b#{JOUR}-*\s*(#{JOUR})*\b}
     SALLES = %r{\b[0-9]+\b}
 
-    FILM = %r{^(#{ID})\s*(#{JOURS})*\s*(#{SALLES})$}
+    FILM = %r{^(#{ID})\s*(#{SALLES})\s*(#{JOURS})*$}
 
   end
 end
